1648555fdd29d7f601fa925063fb0e63eaa5a952
Gabriel Fray Garandy <gabrielfraygarandy@gmail.com>
2022-12-14 16:29:30 -0300

    feat: Add my informations

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Gabriel Fray",
+  githubUser: "GabrielFray",
+  linkedinUser: "gabrielfray",
+  whatsappNumber: "+5519997072653",
 };
